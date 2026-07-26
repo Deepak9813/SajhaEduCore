ead00bd4dbe80e6d4be62c98c8fce6b40e873900
Deepak Baij <deepakbaij0927@gmail.com>
2026-07-26 14:00:10 +0545

    third commit-remove admin default setting

--- config/config/urls.py
+++ config/config/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("api/v1/", include("apps.api.urls")),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
